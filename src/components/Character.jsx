378c608844121437097b5812d43c154b95d5bdf7
Chsebas98 <chsebas98@gmail.com>
2022-07-03 14:58:20 -0500

    rick y morty

---
+++ src/components/Character.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Character = ({ character }) => {
+	return (
+		<div className="text-center p-5">
+			<h3>{character.name}</h3>
+			<img src={character.image} alt="Imagen" className="img-fluid rounded-pill" />
+			<p>{character.origin.name}</p>
+		</div>
+	);
+};
+
+export default Character;
